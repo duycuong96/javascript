17351c0c77fa129cc0ccefd29864c05058384a64
duycuong96 <vuduycuong996@gmail.com>
2019-11-29 11:43:24 +0700

    update

--- ajax-json/demo5/app.js
+++ ajax-json/demo5/app.js
@@ -22,3 +22,25 @@ function getProducts() {
     $.ajax(settings);
 }
 getProducts();
+
+    $(".contact-form").submit(function(){
+        if(name.val() == "" ){
+            name.focus();
+            $('#userError').html("Vui lòng nhập tên");
+            return false;
+        }
+        if ( pattern.test(email.val()) == false ) {{
+            email.focus();
+            $('#emailError').html("Bạn cần nhập đúng email");
+            return false;
+        }}
+    });
+    // kiểm tra nếu nhập thì mất lỗi
+    name.bind('blur', function(){
+        if(name.val() == ""){
+            name.focus();
+            $('#userError').html("Vui lòng nhập tên");
+        } else {
+            $('#userError').html("");
+        }
+    });
--- ass/js/main.js
+++ ass/js/main.js
@@ -1,26 +1,79 @@
 // header
-$(document).ready(function(){
-  var pull        = $('#pull');
-      menu        = $('nav ul');
-      menuHeight  = menu.height();
+$(document).ready(function () {
+  var pull = $('#pull');
+  menu = $('nav ul');
+  menuHeight = menu.height();
 
-    $(pull).on('click', function(e) {
-        e.preventDefault();
-        menu.slideToggle();
-    });
-});
-
-$(window).resize(function(){
-    var w = $(window).width();
-    if(w > 980 && menu.is(':hidden')) {
-        menu.removeAttr('style');
-    }
-});
-
-// slider
-$(window).load(function() {
-    $('.flexslider').flexslider({
-      animation: "slide"
-    });
+  $(pull).on('click', function (e) {
+    e.preventDefault();
+    menu.slideToggle();
   });
+});
+
+$(window).resize(function () {
+  var w = $(window).width();
+  if (w > 980 && menu.is(':hidden')) {
+    menu.removeAttr('style');
+  }
+});
+
+
+
+$(document).ready(function () {
+  // Hàm active tab nào đó
+  function activeTab(obj) {
+    // Xóa class active tất cả các tab
+    $('.tab-wrapper ul li').removeClass('active');
+
+    // Thêm class active vòa tab đang click
+    $(obj).addClass('active');
+
+    // Lấy href của tab để show content tương ứng
+    var id = $(obj).find('a').attr('href');
+
+    // Ẩn hết nội dung các tab đang hiển thị
+    $('.tab-item').hide();
+
+    // Hiển thị nội dung của tab hiện tại
+    $(id).show();
+  }
+
+  // Sự kiện click đổi tab
+  $('.tab li').click(function () {
+    activeTab(this);
+    return false;
+  });
+
+  // Active tab đầu tiên khi trang web được chạy
+  activeTab($('.tab li:first-child'));
+});
+
+
+// list products
+
+function renderProduct(product) {
+  let template = $("#product").html();
+  template = template.replace("{{id}}", product.id);
+  template = template.replace("{{name}}", product.name);
+  template = template.replace("{{price}}", product.price);
+  template = template.replace("{{image}}", product.image);
+  let shopCart = $('.products-row');
+  $(template).appendTo(shopCart);
+}
+
+
+function getProducts() {
+  let settings = {
+      url: "http://5dcf7e2d75f9360014c268b9.mockapi.io/product?page=1&limit=12",
+      dataType: "json",
+      success: function(data, textStatus, jqXHR) {
+          for (var product of data) {
+              renderProduct(product);
+          }
+      }
+  };
+  $.ajax(settings);
+}
+
+getProducts();
 
